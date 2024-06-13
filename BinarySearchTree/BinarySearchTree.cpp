b8b69bac2efbae59d24165e44186781f01c7ea85
muhammadraflyhusen <raflyhusen311@gmail.com>
2024-06-13 16:14:39 +0700

    membuat case 1

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -126,7 +126,20 @@ int main() {
 		char ch;
 		cin >> ch;
 		cout << endl;
-
-
+		
+		switch (ch)
+		{
+			case '1': 
+			{
+				cout << "Enter a word: ";
+				string word;
+				cin >> word;
+				x.insert(word);
+				break;
+			}
+			
+			
+		}
+		
 	}
 }
